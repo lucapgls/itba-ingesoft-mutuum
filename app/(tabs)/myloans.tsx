6b10a3cbef3f2d4ec8169d36f5475352bc94d236
lucapgls <luca.pugliese.ar@gmail.com>
2024-09-02 21:23:56 -0300

    Initial commit

---
+++ app/(tabs)/myloans.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyLoans = () => {
+  return (
+    <View>
+      <Text>MyLoans</Text>
+    </View>
+  )
+}
+
+export default MyLoans
---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import { Stack } from 'expo-router'
+import "../global.css"
+
+
+const RootLayout = () => {
+  return (
+   <Stack>
+    <Stack.Screen name='index' options={{headerShown: false}} />
+    </Stack>
+  )
+}
+
+export default RootLayout
