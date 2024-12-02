71dc0339adf620e63100e8726dbf3837f679bc9f
gonza-sharif2003 <gonzalocuri@gmail.com>
2024-12-02 13:02:11 -0300

    Added .mocharc file

---
+++ .mocharc.cjs
@@ -0,0 +1,21 @@
+// .mocharc.js
+module.exports = {
+    require: ['@babel/register'],
+    env: [
+        'NODE_ENV=test'
+    ],
+    ignore: [
+        'node_modules/**/*'
+    ],
+
+    // config
+    spec: 'testing/*.js', 
+    timeout: 10000,
+    reporter: 'nyan',
+    colors: true,
+    pendingTests: true,
+    fullTrace: true,
+    bail: false,
+    slow: 1000,
+
+};
